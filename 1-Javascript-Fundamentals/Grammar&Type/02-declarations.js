00a575ff4c88c655450f7d59368c9c4095475418
DANellis92 <dvdnellis@gmail.com>
2019-03-05 10:09:30 -0500

    ah

---
+++ 1-Javascript-Fundamentals/Grammar&Type/02-declarations.js
@@ -0,0 +1,38 @@
+let a = 2;
+let A = 3;
+
+//let    a    =     2
+/*(1)  (2)  (3)   (4)
+1 = Keyword
+2 = Variable
+3 = Assignment Opperator
+4 = Variable Opperator
+*/
+
+/**********************
+ * DECLARATIONS*
+ **********************/
+
+
+/*
+Declarations arethe left side of a variable
+    It is simply the let x
+    It is on the left side of the assignment operator (=)
+Initializations are the RIGHT SIDE of a variable
+    It sets the value of the variable
+    It incorporates the variable name (x), the assignment operator (=), and the value
+    let x;
+    console.log('Declaration:',x);
+    x=10;
+    console.log('Initialization:',x);
+    
+    x=33;
+    console.log('Initialization:',x);
+    let today = "Great!";
+    const elevenFifty ="Wonderful!";
+    console.log(today, elevenFifty);
+    
+    elevenFifty ="super";
+    console.log(today,elevenFifty);
+  */
+   
